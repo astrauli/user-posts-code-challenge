13638569860b0df7e380ed63aed7e84452cf71c0
Aaron Strauli <astrauli@gmail.com>
2023-08-14 15:52:21 -0700

    add more JSDocs

--- api/src/repositories/userRepository.ts
+++ api/src/repositories/userRepository.ts
@@ -3,29 +3,72 @@ import CreateUserInput from '../types/CreateUserInput'
 import UpdateUserInput from '../types/UpdateUserInput'
 import { prisma } from '../prisma'
 
+/**
+ * A repository for handling user-related database operations.
+ *
+ * @class
+ */
 class UserRepository {
   userClient: Prisma.UserDelegate
 
+  /**
+   * Constructs a new UserRepository instance.
+   *
+   * @constructor
+   * @param {Prisma.UserDelegate} userClient - The Prisma client for the User model.
+   */
   constructor(userClient: Prisma.UserDelegate) {
     this.userClient = userClient
   }
 
+  /**
+   * Creates a new user.
+   *
+   * @param {CreateUserInput} user - The user data for creation.
+   * @returns {Promise<User>} The created user.
+   */
   async createUser(user: CreateUserInput): Promise<User> {
     return await this.userClient.create({ data: user })
   }
 
+  /**
+   * Retrieves a user by their ID.
+   *
+   * @param {number} id - The ID of the user to retrieve.
+   * @returns {Promise<User>} The retrieved user.
+   */
   async getUserById(id: number): Promise<User> {
     return await this.userClient.findUnique({ where: { id } })
   }
 
+  /**
+   * Updates a user by their ID.
+   *
+   * @param {number} id - The ID of the user to update.
+   * @param {UpdateUserInput} data - The updated user data.
+   * @returns {Promise<User>} The updated user.
+   */
   async updateUserById(id: number, data: UpdateUserInput): Promise<User> {
     return await this.userClient.update({ where: { id }, data })
   }
 
+  /**
+   * Deletes a user by their ID.
+   * Deleting a user will delete all of their posts.
+   *
+   * @param {number} id - The ID of the user to delete.
+   * @returns {Promise<User>} The deleted user.
+   */
   async deleteUserById(id: number): Promise<User> {
     return await this.userClient.delete({ where: { id } })
   }
 
+  /**
+   * Retrieves posts associated with a user.
+   *
+   * @param {number} userId - The ID of the user.
+   * @returns {Promise<Post[]>} The posts associated with the user.
+   */
   async getUserPosts(userId: number): Promise<Post[]> {
     const user = await this.userClient.findUnique({
       where: {
@@ -44,6 +87,11 @@ class UserRepository {
   }
 }
 
+/**
+ * Returns a default instance of UserRepository with a default Prisma user client.
+ *
+ * @returns {UserRepository} A default UserRepository instance.
+ */
 export const getDefaultUserRepository = (): UserRepository => {
   return new UserRepository(prisma.user)
 }
--- api/src/util/validations/index.ts
+++ api/src/util/validations/index.ts
@@ -1,3 +1,9 @@
+/**
+ * Validates whether an email address is in a valid format.
+ *
+ * @param {string} email - The email address to validate.
+ * @returns {boolean} true if the email is valid, otherwise false.
+ */
 export const isValidEmail = (email: string): boolean => {
   const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
 
