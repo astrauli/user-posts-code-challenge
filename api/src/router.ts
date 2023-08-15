856d244ae25c05f4ee6e88952e4e64a71c729cda
Aaron Strauli <astrauli@gmail.com>
2023-08-14 17:21:09 -0700

    add cors to express

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,10 +1,12 @@
 import BodyParser from 'body-parser'
 import Router from 'express-promise-router'
+import cors from 'cors'
 import UserRoute from './routes/userRoute'
 import PostRoute from './routes/postRoute'
 
 const router = Router()
 
+router.use(cors())
 router.use(BodyParser.json())
 
 router.use('/api/users', UserRoute)
